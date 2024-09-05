67972b26bdf2dc22606ce9374553073c536c1cab
Jefferson <jlavadoastuhuaman@gmail.com>
2024-09-04 23:35:49 -0500

    primer commit

---
+++ Lab1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cartel Científico</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="encabezado1">
+        <h1>Título del Cartel Científico</h1>
+    </div>
+    <br>
+    <div class="encabezado2">
+        <h1>Autores</p>
+    </div>
+    <br>
+    <div class="encabezado2">
+        <h1>Presentación</p>
+    </div>
+    <br>
+    <div class="contenido">
+        <div class="seccion izquierda">
+            <h1>Contribuciones</h2>
+        </div>
+        <div class="seccion derecha">
+            <h1>Bibliografía</h2>
+        </div>
+    </div>
+    <br>
+    <div class="respuesta">
+        <h1>Respuesta</h1>
+    </div>
+</body>
+
+</html>
